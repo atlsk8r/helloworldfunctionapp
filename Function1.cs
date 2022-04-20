ef6c1647e0e017ffb6af6f360e91453e0986ec4d
atlsk8r <86693461+atlsk8r@users.noreply.github.com>
2022-04-20 14:50:48 -0400

    Add deleteme comment

--- Function1.cs
+++ Function1.cs
@@ -27,7 +27,7 @@ namespace HelloWorldFunctionApp
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully. *Please DeleteMe Comment!*";
 
             return new OkObjectResult(responseMessage);
         }
